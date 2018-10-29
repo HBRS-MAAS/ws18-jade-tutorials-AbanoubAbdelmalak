795cf776faf636274e31552ea7f799d94c370c15
Abanoub Abdelmalak <abanoub_157@yahoo.com.com>
2018-10-29 20:00:38 +0100

    Solve the problem of closing down

--- src/main/java/maas/tutorials/BookSellerAgent.java
+++ src/main/java/maas/tutorials/BookSellerAgent.java
@@ -1,11 +1,17 @@
 package maas.tutorials;
 
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.basic.Action;
 import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.JADEAgentManagement.JADEManagementOntology;
+import jade.domain.JADEAgentManagement.ShutdownPlatform;
 
 import java.util.*;
 import jade.lang.acl.ACLMessage;
@@ -55,6 +61,20 @@ public class BookSellerAgent extends Agent {
     addBehaviour(new TickerBehaviour(this, 30000) {
       protected void onTick() {
         if (purchaseFlag && serverFlag) {
+          ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
+          Codec codec = new SLCodec();
+          myAgent.getContentManager().registerLanguage(codec);
+          myAgent.getContentManager().registerOntology(JADEManagementOntology.getInstance());
+          shutdownMessage.addReceiver(myAgent.getAMS());
+          shutdownMessage.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+          shutdownMessage.setOntology(JADEManagementOntology.getInstance().getName());
+          try {
+              myAgent.getContentManager().fillContent(shutdownMessage,new Action(myAgent.getAID(), new ShutdownPlatform()));
+              myAgent.send(shutdownMessage);
+          }
+          catch (Exception e) {
+              //LOGGER.error(e);
+          }
           myAgent.doDelete();
         }
       }
@@ -135,6 +155,25 @@ public class BookSellerAgent extends Agent {
         purchaseFlag = true;
         block();
       }
+    }
+  }
+  private class shutdown extends OneShotBehaviour{
+    public void action() {
+      ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
+      Codec codec = new SLCodec();
+      myAgent.getContentManager().registerLanguage(codec);
+      myAgent.getContentManager().registerOntology(JADEManagementOntology.getInstance());
+      shutdownMessage.addReceiver(myAgent.getAMS());
+      shutdownMessage.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+      shutdownMessage.setOntology(JADEManagementOntology.getInstance().getName());
+      try {
+          myAgent.getContentManager().fillContent(shutdownMessage,new Action(myAgent.getAID(), new ShutdownPlatform()));
+          myAgent.send(shutdownMessage);
+      }
+      catch (Exception e) {
+          //LOGGER.error(e);
+      }
+
     }
   }
   
