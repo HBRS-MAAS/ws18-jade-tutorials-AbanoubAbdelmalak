3ad270a17f112ca8faa4996c09b8598f0b45282f
Abanoub Abdelmalak <abanoub_157@yahoo.com.com>
2018-10-29 14:16:51 +0100

    Edit the main method to create one buyer and three sellers for the simple example

--- src/main/java/maas/Start.java
+++ src/main/java/maas/Start.java
@@ -2,24 +2,31 @@ package maas;
 
 import java.util.List;
 import java.util.Vector;
-import maas.tutorials.BookBuyerAgent;
+
+import jade.wrapper.AgentController;
 
 public class Start {
     public static void main(String[] args) {
     	List<String> agents = new Vector<>();
-    	agents.add("tester:maas.tutorials.BookBuyerAgent");
-    	for (int i = 0; i <=19;i++) {
-    		agents.add("buyer"+i+":maas.tutorials.BookBuyerAgent");
+    	List<String> cmd = new Vector<>();
+    	agents.add("seller1:maas.tutorials.BookSellerAgent");
+    	cmd.add("-agents");cmd.add("-agents");cmd.add("-agents");
+    	agents.add("seller2:maas.tutorials.BookSellerAgent");
+    	agents.add("seller3:maas.tutorials.BookSellerAgent");
+    	for (int i = 0; i <=0;i++) {
+    		agents.add("buyer"+i+":maas.tutorials.BookBuyerAgent("+"BMW "+")");
+    		cmd.add("-agents");
     	}
 
-    	List<String> cmd = new Vector<>();
-    	cmd.add("-agents");
+    	
+    	//cmd.add("-agents");
+    	//cmd.add("-args");
     	StringBuilder sb = new StringBuilder();
     	for (String a : agents) {
     		sb.append(a);
     		sb.append(";");
     	}
     	cmd.add(sb.toString());
-        jade.Boot.main(cmd.toArray(new String[cmd.size()]));
+      jade.Boot.main(cmd.toArray(new String[cmd.size()]));
     }
 }
